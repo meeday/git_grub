359c6b18bcb699df7ed1269e32fe3265c962b5c6
meeday <59993824+meeday@users.noreply.github.com>
2020-07-01 21:10:02 +0100

    recipe model created

---
+++ models/Recipe.js
@@ -0,0 +1,52 @@
+const { DataTypes } = require('sequelize');
+
+const sequelize = require('../config/db').sequelize();
+
+const Recipe = sequelize.define('Recipe', {
+  id: {
+    type: DataTypes.STRING,
+    unique: true,
+    primaryKey: true,
+  },
+  googleId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  recipeId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  summary: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  cuisine: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  vegan: {
+    type: DataTypes.BOOLEAN,
+    allowNull: true,
+  },
+  vegetarian: {
+    type: DataTypes.BOOLEAN,
+    allowNull: true,
+  },
+  imageUrl: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    validate: {
+      isUrl: true,
+    },
+  },
+  comments: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+});
+
+module.exports = Recipe;
