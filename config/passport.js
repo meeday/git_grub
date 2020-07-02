1b250abb5e02f68e78910d21ef166b5edaeaaaf3
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-07-02 23:20:52 +0100

    Added parameters to google callback

--- config/passport.js
+++ config/passport.js
@@ -1,15 +1,23 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
+const User = require('../models/User');
 require('dotenv').config();
 
+// Setup of Google Strategy
 passport.use(
   new GoogleStrategy({
-    callbackURL: 'auth/google/redirect',
+    callbackURL: '/auth/google/redirect',
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  },
-  () => {
-    // passport callback function
+  }, (accessToken, refreshToken, profile, done) => {
+    console.log(profile);
   }),
 );
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => done(err, user));
+});
