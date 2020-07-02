173f9039d1343c27cf55d1bb3d8dd1485b9e0c3c
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-07-02 22:41:40 +0100

    Passport setup and Google strategy

---
+++ config/passport.js
@@ -0,0 +1,15 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+require('dotenv').config();
+
+passport.use(
+  new GoogleStrategy({
+    callbackURL: 'auth/google/redirect',
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  },
+  () => {
+    // passport callback function
+  }),
+);
