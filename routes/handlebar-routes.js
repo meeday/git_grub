faf8cc74b0e048ac18f2840ced752f521822b40b
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-07-04 12:13:16 +0100

    Updated route endpoints

--- routes/handlebar-routes.js
+++ routes/handlebar-routes.js
@@ -3,7 +3,15 @@ const { Router } = require('express');
 const router = require('express').Router();
 
 router.get('/guest', (req, res) => {
-    res.render('index');
+    res.render('guest');
+});
+
+router.get('/member', (req, res) => {
+    res.render('member');
+});
+
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard');
 });
 
 module.exports = router;
