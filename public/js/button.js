a130f930ec7e52e1b7164577c33c5b6eb1f38a9f
meeday <59993824+meeday@users.noreply.github.com>
2020-07-04 22:02:36 +0100

    js function clean up

--- public/js/button.js
+++ public/js/button.js
@@ -1,21 +1,7 @@
-// Menu btn
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems);
-});
-
-$(document).ready(function(){
+$(document).ready(() => {
   $('.sidenav').sidenav();
 });
 
-// Cuisine Drop Down
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('select');
-  var instances = M.FormSelect.init(elems);
-});
-
-// Or with jQuery
-
-$(document).ready(function(){
+$(document).ready(() => {
   $('select').formSelect();
 });
