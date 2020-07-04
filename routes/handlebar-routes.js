38ac323c0e3cece14a57565770c8090c75d7403b
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-07-04 14:16:31 +0100

    Added auth checks to routes

--- routes/handlebar-routes.js
+++ routes/handlebar-routes.js
@@ -1,9 +1,16 @@
-const { Router } = require('express');
-
 const router = require('express').Router();
+const { authCheck, guestCheck } = require('../middleware/auth');
 
-router.get('/guest', (req, res) => {
-    res.render('index');
+router.get('/guest', guestCheck, (req, res) => {
+  res.render('guest');
+});
+
+router.get('/member', authCheck, (req, res) => {
+  res.render('member');
+});
+
+router.get('/dashboard', authCheck, (req, res) => {
+  res.render('dashboard');
 });
 
 module.exports = router;
