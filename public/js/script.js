73595967f4102f73ee2cc1a2ac9830cf97257271
meeday <59993824+meeday@users.noreply.github.com>
2020-07-06 14:24:33 +0100

    ajax post request made

--- public/js/script.js
+++ public/js/script.js
@@ -1,25 +1,21 @@
-// const config = require('../../config/config');
-// const api = require('../../utils/axios');
 const searchBtn = $('#search-icon');
 const searchQuery = $('#search-query');
-const cuisineSearch = $('.cuisine');
-const dietSearch = $('.diet');
-const intolerance = $('.intolerance');
 
 searchBtn.on('click', () => {
   const searchTerm = searchQuery.val().trim();
-  const cuisinePref = cuisineSearch.children('option:selected').html();
-  const dietPref = dietSearch.children('option:selected').html();
-  const allergies = intolerance.children('option:selected').html();
-
-  console.log(searchTerm);
-  console.log(cuisinePref);
-  console.log(dietPref);
-  console.log(allergies);
+  const cuisinePref = ($('.cuisine option:selected').toArray().map((item) => item.text)).toString();
+  const dietPref = ($('.diet option:selected').toArray().map((item) => item.text)).toString();
+  const allergies = ($('.intolerance option:selected').toArray().map((item) => item.text)).toString();
+  //  console.log(searchTerm);
+  const preferences = {
+    term: searchTerm,
+    cuisine: cuisinePref,
+    diet: dietPref,
+    allergy: allergies,
+  };
+  $.ajax({
+    url: '/api/search',
+    method: 'POST',
+    data: preferences,
+  }).then((result) => console.log(result));
 });
-
-// console.log(cuisineSearch);
-// console.log(dietSearch);
-// console.log(intolerance);
-
-// api.userSearch(searchQuery);
