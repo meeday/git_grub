597ef8a0c6a1e142e6c9bf3c268ecfd5474adf8a
meeday <59993824+meeday@users.noreply.github.com>
2020-07-06 14:25:03 +0100

    file structure linted

--- routes/handlebar-routes.js
+++ routes/handlebar-routes.js
@@ -3,15 +3,15 @@ const { Router } = require('express');
 const router = require('express').Router();
 
 router.get('/guest', (req, res) => {
-    res.render('guest');
+  res.render('guest');
 });
 
 router.get('/member', (req, res) => {
-    res.render('member');
+  res.render('member');
 });
 
 router.get('/dashboard', (req, res) => {
-    res.render('dashboard');
+  res.render('dashboard');
 });
 
 module.exports = router;
