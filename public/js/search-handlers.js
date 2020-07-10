37b9e75de7431fa9e18ffc0fc0d7383a0916d05e
ian <chengkwanyu@yahoo.com.hk>
2020-07-10 11:16:52 +0800

    deleted unused script

--- public/js/search-handlers.js
+++ public/js/search-handlers.js
@@ -8,20 +8,3 @@ $(document).ready(() => {
   $('select').formSelect();
 });
 
-// Search Selection
-const cuisineDropdown = $('#cuisine-dropdown');
-const intoleranceDropdown = $('#intolerance-dropdown');
-const dietDropdown = $('#diet-dropdown');
-
-// Search Filter Function
-cuisineBtn.on('click', () => {
-  dietDropdown.hide();
-  intoleranceDropdown.show();
-  cuisineDropdown.show();
-});
-
-dietBtn.on('click', () => {
-  cuisineDropdown.hide();
-  intoleranceDropdown.show();
-  dietDropdown.show();
-});
